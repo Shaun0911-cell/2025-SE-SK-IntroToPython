0484c10547b1027e1311880137b2beb0d65a6140
Shaun0911-cell <Shaun.kim202@education.nsw.gov.au>
2025-03-17 22:43:07 +0000

    getting indoor code to work

--- src/function_variables/indoor.py
+++ src/function_variables/indoor.py
@@ -1,6 +1,8 @@
 def indoor():
     # This code is an example and should be replaced with your own solution
-    print("Hello, world!")
+    value = input("What is the word?")
+    print(value.lower())
+
 
 if __name__ == "__main__":
     indoor()
